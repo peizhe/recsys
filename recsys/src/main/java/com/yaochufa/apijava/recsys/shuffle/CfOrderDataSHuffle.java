17df2d8effe781588e07a6110a3f4570eeeb1392
xuhaitao <taotoxht@163.com>
2015-12-18 18:58:28 +0800

    11

--- recsys/src/main/java/com/yaochufa/apijava/recsys/shuffle/CfOrderDataSHuffle.java
+++ recsys/src/main/java/com/yaochufa/apijava/recsys/shuffle/CfOrderDataSHuffle.java
@@ -21,6 +21,7 @@ import org.apache.spark.api.java.function.PairFlatMapFunction;
 import org.apache.spark.api.java.function.VoidFunction;
 
 import scala.Tuple2;
+import scala.specialized;
 
 import com.yaochufa.apijava.recsys.util.GlobalVar;
 
@@ -36,19 +37,22 @@ import com.yaochufa.apijava.recsys.util.GlobalVar;
 public class CfOrderDataSHuffle implements DataShuffle {
 
 	private static final Pattern COMMA = Pattern.compile(",");
+	private final Pattern JH = Pattern.compile("#");
 	private String inputPath;
-
-	public CfOrderDataSHuffle(String inputPath) {
+	private String freqPatternDataPath;
+	
+	public CfOrderDataSHuffle(String inputPath,String freqPatternDataPath) {
 		super();
 		this.inputPath = inputPath;
+		this.freqPatternDataPath = freqPatternDataPath;
 	}
 
 	@Override
 	public void shuffle(JavaSparkContext jssc) {
-		String fileName = new SimpleDateFormat("yyyyMMdd").format(new Date())+".csv";
+		String fileNamePrefix = new SimpleDateFormat("yyyyMMdd").format(new Date());
 //		jobConf.setOutputFormat(theClass);
 //		jobConf.set(DBoutputFormat,"user");JavaRDD<String> data=
-		jssc.textFile(inputPath + "/" + fileName)
+		JavaRDD<String> data=jssc.textFile(inputPath + "/" + fileNamePrefix+".csv")
 				.filter(new Function<String, Boolean>() {
 
 					@Override
@@ -56,12 +60,37 @@ public class CfOrderDataSHuffle implements DataShuffle {
 						String[] tok = COMMA.split(line);
 						return tok.length >= 4;
 					}
-				}).foreachPartition(new OutputFunction());
+				}).cache();
+		data.foreachPartition(new HbaseOutputFunction());
+		data.map(new FileOutputFunction()).saveAsTextFile(freqPatternDataPath+"/"+fileNamePrefix);
 	}
+     
+	class FileOutputFunction implements Function<String, String>{
 
+		@Override
+		public String call(String line) throws Exception {
+			String arr[] = COMMA.split(line);
+			String userId = arr[1];
+			String[] productIds = JH.split(arr[2]);
+			return join(productIds, " ");
+		}
+		
+		private <T> String join(T[] list,String separator){
+			StringBuilder sb=new StringBuilder();
+			for(T t :list){
+				sb.append(t).append(separator);
+			}
+			if(sb.length()>0){
+				return sb.substring(0, sb.length()-1);
+			}else{
+				return sb.toString();
+			}
+		}
+	}
+	
 	class OutputPairFlatMapFunction implements PairFlatMapFunction<String, ImmutableBytesWritable, Put>{
 
-		private final Pattern COMM = Pattern.compile("#");
+		
 		private final String FORMET = "yyyyMMddHHmmss";
 		private long THREE_MONTH_DIFF = 3 * 30 * 24 * 3600000;
 		private long SIX_MONTH_DIFF = 6 * 30 * 24 * 3600000;
@@ -107,36 +136,36 @@ public class CfOrderDataSHuffle implements DataShuffle {
 		
 	}
 	
-	class OutputFunction implements VoidFunction<Iterator<String>> {
+	class HbaseOutputFunction implements VoidFunction<Iterator<String>> {
 
-		private final Pattern COMM = Pattern.compile("#");
 		private final String FORMET = "yyyyMMddHHmmss";
 		private long THREE_MONTH_DIFF = 3 * 30 * 24 * 3600000;
 		private long SIX_MONTH_DIFF = 6 * 30 * 24 * 3600000;
 		private long TWELVE_MONTH_DIFF = 12 * 30 * 24 * 3600000;
 		private long currTime = new Date().getTime();
 		private final int baseOrderScore = 30;
-		private final String TABLE_NAME="usert_product_score";
 		
 		@Override
 		public void call(Iterator<String> t) throws Exception {
 			Configuration conf=HBaseConfiguration.create();
 			conf.set("hbase.zookeeper.property.clientPort", "2181");
 			conf.set("hbase.zookeeper.quorum", "192.168.9.113");
-			HTable table=new HTable(conf, TABLE_NAME);
+			HTable user_product_score=new HTable(conf, "user_product_score");
+			HTable product_item_set=new HTable(conf, "product_item_set");
 			while (t.hasNext()) {
 				String arr[] = COMMA.split(t.next());
 				String userId = arr[1];
-				String[] productIds = COMMA.split(arr[2]);
+				String[] productIds = JH.split(arr[2]);
 				long timestamp = Long.parseLong(arr[3]);
 				double score = baseOrderScore;
 				score = timeBoost(score, timestamp);
 				int isCancled = Integer.parseInt(arr[4]);
 				// score=stateBoost(score,isCancled);
+//				Put post=new Put((userId+"_"+productId).getBytes());
 				for (String productId : productIds) {
 					Put p=new Put((userId+"_"+productId).getBytes());
 					p.addColumn("cf1".getBytes(),  "score".getBytes(),String.valueOf(score).getBytes() );
-					table.put(p);
+					user_product_score.put(p);
 				}
 
 			}
@@ -165,6 +194,6 @@ public class CfOrderDataSHuffle implements DataShuffle {
 		SparkConf conf = new SparkConf().setAppName("Collaborative Filtering with Mysql").set("spark.master", GlobalVar.SPARKCONF_MASTER);
 		conf.set("spark.executor.memory", "1024M");
 		JavaSparkContext jssc = new JavaSparkContext(conf);
-		new CfOrderDataSHuffle("data/csv/order").shuffle(jssc);
+		new CfOrderDataSHuffle("data/csv/order","data/csv/freq_pattern").shuffle(jssc);
 	}
 }
